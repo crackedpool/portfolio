3b8e4e372672720b6e0892b213e039083f1ff7bd
Rakhmatullaev Abdulaziz <frrhyk@gmail.com>
2025-06-12 00:36:28 +0500

    do ne

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -17,8 +17,8 @@ export default async function Page({
 
   return (
     <div className={`flex flex-col gap-[15px] ${project.images ? "" : "justify-center"} w-full relative`}>
-      <div className='flex justify-center'>
-        <Link href="/" className='absolute bb left-0 flex gap-1 items-center cursor-pointer hover:opacity-50'>
+      <div className='flex justify-center items-center sticky top-0 bg-[var(--background-color)]/40 border-b border-[var(--background-color)]/20 py-2 backdrop-blur-sm'>
+        <Link href="/" className='absolute bb left-2 inset-y-0 flex gap-1 items-center cursor-pointer hover:opacity-50'>
           <svg width="13" height="13" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg">
             <g id="Page-2" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
               <g id="back_24">
