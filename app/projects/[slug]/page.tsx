1cd4557efc36cbf079ba7a48d3d9f653948b769f
Crackedpool <rakhmatullaevabdulaziz@gmail.com>
2025-06-24 09:26:06 +0500

    redesign 2.1

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -2,7 +2,6 @@ import { notFound } from 'next/navigation'; // To handle invalid slugs
 import { projs } from '../../../data/projects';
 import Link from 'next/link';
 import Image from 'next/image';
-import { div } from 'framer-motion/client';
 
 export default async function Page({
   params,
@@ -34,39 +33,49 @@ export default async function Page({
           ))}
         </div>
       ) : null}
-      <div className='w-1/2 max-h-full relative flex flex-col gap-3'>
-        <Link href={'/'} className='border hover:bg-orange-500/20 w-1/2 p-2 text-[var(--clay)]'>
-          Back
-        </Link>
+      <div className='w-1/2 flex flex-col relative gap-3'>
         <div className='inf flex flex-col gap-1 p-6 bg-[var(--cement)] text-[var(--carbon)]'>
-          <div className='mb-5 text-[25px]'>{project.name}</div>
+          <div className='flex mb-5 text-[17px] gap-2 justify-between items-center'>
+            <div>{project.name}</div>
+            <div className='mr-8 text-sm'>{projs.findIndex((p) => p.slug === slug) + 1} / {projs.length}</div>
+          </div>
           <div className='flex justify-between items-center'>
             {project.role ? (
               <div className='flex flex-col gap-1'>
-                <div className='uppercase'># Role</div>
+                <div className='uppercase'>## Role</div>
                 {project.role}
               </div>
             ) : null}
             {project.emp_type ? (
               <div className='flex flex-col gap-1 pr-8'>
-                <div className='uppercase'># employment type</div>
+                <div className='uppercase'>## employment type</div>
                 {project.emp_type}
               </div>
             ) : null}
           </div>
+          {project.dur ? (
+            <div className='flex flex-col gap-1'>
+              <div className='uppercase mt-3'>## Duration</div>
+              {project.dur}
+            </div>
+          ) : null}
           {project.tech_stack ? (
             <div className='flex flex-col gap-1'>
-              <div className='uppercase mt-3'># Tech stack</div>
+              <div className='uppercase mt-3'>## Tech stack</div>
               {project.tech_stack}
             </div>
           ) : null}
+          <div className='my-5'>---</div>
           {project.description ? (
             <div className='flex flex-col gap-1'>
-              <div className='uppercase mt-3'># About</div>
+              <div className='uppercase'>## About</div>
               {project.description}
             </div>
           ) : null}
         </div>
+        <Link href={'/'} className='sticky top-3 border hover:bg-green-600/20 w-1/2 p-2 text-[var(--clay)]'>
+          Back
+        </Link>
       </div>
     </div >
   )
--- data/projects.ts
+++ data/projects.ts
@@ -8,6 +8,14 @@ export const projs = [
     role: "Frontend Developer",
     emp_type: "Full-time",
     tech_stack: "Vite, React, TypeScript",
+    dur: "Jun 2025 - Present"
+  },
+  {
+    slug: "maab",
+    name: 'MAAB',
+    description: "a Tashkent-based IT organization specializing in Business Intelligence services. Our company was founded by a team of professionals with strong expertise in both business and technology, and we are dedicated to providing tailored Business Intelligence solutions for various industry organizations and institutions.",
+    role: 'Data engineer',
+    dur: 'Aug 2023 - Jan 2025'
   },
   {
     slug: "peace",
@@ -15,6 +23,9 @@ export const projs = [
     // description: "Peace is my first real project where I started with my friend Dennis, it was his idea, while every detail was on me, from designing to coding the platform. We were thinking about worldwide problems, and the solution was this platform Peace, where the purpose is to help people in need, any person can ask any question and the one who knows it answers, but I wanted to make people stay there, so it gradually became to a more common platforms like Instagram and primarily X, where my app would be also place of free speech. As I'm working along on this project, I have only unfinished code. But soon, In Sha Allah it will be public.",
     description: "Peace is an inclusive online platform dedicated to fostering a deeper understanding of Islam, while promoting respect for all faiths. The website offers users access to authentic Islamic teachings, including Sahih Hadith, and real-life miracle stories. With AI integration in the backend, Peace ensures the authenticity of posts, automatically verifying that content is free from Haram elements and preventing the spread of fabricated Hadith. The platform encourages community engagement through a unique leaderboard system, where users earn recognition for contributing valuable content.By sharing Hadith or personal stories, users help grow the website’s knowledge base and, in return, climb the leaderboard rankings based on the popularity of their posts.The top contributors receive rewards, fostering a sense of motivation and active participation.This project aims to create a respectful, interactive space where individuals can learn, share, and connect while deepening their connection to Islam.",
     images: ["/peace/1.png", "/peace/2.png", "/peace/3.png", '/peace/4.png'],
+    tech_stack: "NextJS, React, TypeScript",
+    role: "founder",
+    dur: "Mar 2025 - Present"
   },
   {
     slug: "crackedzone",
@@ -23,6 +34,8 @@ export const projs = [
     images: ["/crkdzone/1.png", "/crkdzone/2.png", "/crkdzone/3.png", "/crkdzone/4.png", "/crkdzone/5.png", "/crkdzone/6.png"],
     device: "phone",
     tech_stack: "NextJS, React, TypeScript, Tailwind CSS",
+    role: "founder",
+    dur: "Feb 2024 - March 2025"
   },
   {
     slug: "faceid",
@@ -31,7 +44,8 @@ export const projs = [
     images: ["/fcid/01.jpeg", "/fcid/02.jpeg", "/fcid/03.jpeg", "/fcid/04.jpeg", "/fcid/05.jpeg"],
     role: "Software Engineer",
     emp_type: "Contract",
-    date: "less than a day"
+    tech_stack: "Python, OpenCV, DeepFace",
+    dur: '1 day'
   },
   {
     slug: "flight-centre",
@@ -42,6 +56,7 @@ export const projs = [
             3. Improved the efficiency of data fetching using indexing and query optimization`,
     role: "Data Engineer",
     emp_type: "Contract",
+    dur: 'Sep 2023 - Dec 2023'
   },
   {
     slug: "sonnys-carwash",
@@ -52,6 +67,7 @@ export const projs = [
             3 - Optimized and maintained data architecture built around automated ingestion`,
     role: "Data Engineer",
     emp_type: "Contract",
+    dur: 'Nov 2023 - Dec 2023'
   },
   {
     slug: "houz",
@@ -59,5 +75,6 @@ export const projs = [
     description: "An online marketplace for short- and long-term homestays, inspired by Airbnb, designed with a simplified and localized user interface tailored to the needs of users in my country. The platform acts as a broker, facilitating bookings and generating revenue through a commission-based model.",
     images: ["/houz/vid.gif", "/houz/01.png", "/houz/02.png", "/houz/03.png", "/houz/04.png"],
     tech_stack: "Pyton - Django",
+    role: "founder"
   },
 ];
